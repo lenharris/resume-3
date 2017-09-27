091acfe7085faa56a4e5d45cae69dc2917bacd1f
lenharris <stacia.lt.harris@gmail.com>
2017-09-27 09:10:09 -0700

    Javascript

---
+++ script.js
@@ -0,0 +1,42 @@
+//Name//
+var name = 'Len Harris';
+console.log(name.toUpperCase());
+//Career//
+var career = 'Venture Developer';
+console.log(career);
+//Short Desciption//
+var whatever = 'freedom';
+console.log('I like'+' ' + whatever+'.');
+//List of interest//
+var myinterest = ['Hiking', 'Roadtrips', 'Yoga', 'Sleeping on the Beach'];
+console.log(myinterest);
+//Previous Experience//
+var displayPosition = function(companyName, jobTitle, description) {
+    console.log(companyName, jobTitle, description);
+}
+displayPosition("Ruth Chris", "*Server", "*Help guest");
+displayPosition("Ben & Jerry", "*Ice Cream Taster", "*Eat ice cream" );
+displayPosition("Innovate BHAM", "*Student Developer", "*Take I AM BHAM Class");
+//Skills//
+ console.log('My Skills');
+
+
+var frenchHorn = 'cool';
+
+if (frenchHorn === 'cool') {
+    console.log('Check it: I play the french horn.');
+} else {
+    console.log('I can barely do Adobe Illustrator');
+};
+var roadTrippin = 'cool';
+if (roadTrippin === 'cool') {
+    console.log('Check it: I am the perfect road trip driver.');
+} else {
+    console.log('I might fall asleep on you during a road trip.');
+};
+var competitiveEater = 'not cool'
+if (competitiveEater === 'cool') {
+ console.log('Check it: I totally competitively eat.')
+} else {
+    console.log('I do not competitively eat, but I can grow plants.')
+}
